f92a299dd4b7ea3b4f2bee942fc990c10f6ad571
Alexander <gajicav@mail.uc.edu>
2023-10-26 14:58:51 -0400

    Connected Database

--- src/main/java/com/soit23/enterprise/FacultyController.java
+++ src/main/java/com/soit23/enterprise/FacultyController.java
@@ -1,6 +1,7 @@
 package com.soit23.enterprise;
 
 import com.soit23.enterprise.entity.Faculty;
+import com.soit23.enterprise.service.FacultyService;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,44 +15,28 @@ import java.util.ArrayList;
 @RequestMapping("/Faculties")
 public class FacultyController {
 
-    //Upload Faculty Info
-    private List<Faculty> theFaculties;
+public FacultyService facultyService;
+
+public FacultyController (FacultyService theFacultyService){
+    facultyService = theFacultyService;
 
 
-    //Mapping for "/lists"
 
+}
+
+
+
+//Mapping for list
     @GetMapping("/list")
     public String listFaculties(Model theModel){
 
-
-        //Add Faculties to the Spring Model
+        //Retrieve faculties from the database
+        List<Faculty> theFaculties = facultyService.findAll();
+    //Add Faculties to the Sprig Model
         theModel.addAttribute("faculties", theFaculties);
 
         return "list-faculties";
 
     }
-
-
-
-   @PostConstruct
-    private void loadData(){
-
-       //Create Faculties
-       Faculty fac1 = new Faculty(1, "Kelly", "Miller", "Assistant Professor", "kelly@uc.edu");
-       Faculty fac2 = new Faculty(2, "Robert", "Lee", "Instructor-Educator", "Robert@uc.edu");
-       Faculty fac3 = new Faculty(3, "Laura", "West", "Adjunct-Professor", "Laura@uc.edu");
-
-
-       //Create our List
-       theFaculties = new ArrayList<>();
-
-
-       //Add to our List
-       theFaculties.add(fac1);
-       theFaculties.add(fac2);
-       theFaculties.add(fac3);
-    }
-
-
 
 }
